6ed3dda704f2f24ebd56aef64a4dd006e5369ac0
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-07-05 16:37:01 +0300

    add

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,7 @@ import PostItem from "./PostItem"
 
 const PostsList = ({posts}) => {
   return <ul className="flex flex-col gap-10 basis-4/5">
-    {posts?.map(item => {
-      if(item) {
-        return <PostItem key={item._id} item={item}/>
-      }
-    }
+    {posts?.map(item => item && <PostItem key={item._id} item={item}/>
     )}
   </ul>
 }
--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -2,11 +2,7 @@ import UserPostsItem from "./UserPostsItem"
 
 const UserPosts = ({posts}) => {
   return <ul className="flex flex-col gap-4 py-5">
-  {posts?.map(post => {
-    if(post){
-      return <UserPostsItem key={post._id} post={post}/>
-    }
-  })}
+  {posts?.map(post => post && <UserPostsItem key={post._id} post={post}/> )}
 </ul>
 }
 
